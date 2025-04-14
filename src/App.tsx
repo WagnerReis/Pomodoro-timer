5906998b0a7af5125af846202abb5853a7aa1d74
Wagner Reis <wagnerreis1997@gmail.com>
2025-04-14 05:51:58 -0300

    feat(ui): add Button component with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="success">Success</Button>
+      <Button variant="danger">Danger</Button>
+      <Button>Custom</Button>
+    </>
   )
 }
