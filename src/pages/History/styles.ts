fdd21fb8ff13b693d2274d6bfd52c140b069e761
Wagner Reis <wagnerreis1997@gmail.com>
2025-04-15 16:37:28 -0300

    feat(Home): add cycle management functionality

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -81,7 +81,6 @@ export const Status = styled.span<StatusProps>`
     width: 0.5rem;
     height: 0.5rem;
     border-radius: 50%;
-    background: ${({ theme, statusColor }) =>
-      theme[STATUS_COLORS[statusColor]]};
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
   }
 `;
