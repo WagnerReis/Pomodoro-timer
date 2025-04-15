0de0274c3dedbc4e7774207539f416d5e7c6d659
Wagner Reis <wagnerreis1997@gmail.com>
2025-04-15 06:34:33 -0300

    feat(history): implement History page layout and styling

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
-import { Routes, Route } from "react-router-dom";
-import { History } from "./pages/History";
+import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
+import { History } from "./pages/History";
 import { Home } from "./pages/Home";
 
 export function Router() {
