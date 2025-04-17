309c2d5b578a53738c713b3d58e5d9042013a5e1
Wagner Reis <wagnerreis1997@gmail.com>
2025-04-17 08:56:35 -0300

    chore(eslint): disable react-refresh/only-export-components rule

--- eslint.config.js
+++ eslint.config.js
@@ -26,6 +26,7 @@ export default tseslint
           { allowConstantExport: true },
         ],
         "@typescript-eslint/no-explicit-any": "off",
+        "react-refresh/only-export-components": "off",
       },
     },
   )
